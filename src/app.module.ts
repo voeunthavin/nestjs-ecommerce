78ab126f8e4f6cce62f97c6d980d7fdf22b02795
voeunthavin86 <voeunthavin86@gmail.com>
2022-07-13 10:24:51 +0700

    Add mongodb atlas cluster

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,14 @@ import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/store'), ProductModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://cluster0.ilxtr.mongodb.net', {
+    user: process.env.MONGODB_USER,
+    pass: process.env.MONGODB_PASS,
+    dbName: process.env.MONGODB_NAME,
+    w: 'majority',
+    retryWrites: true
+  }), ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
